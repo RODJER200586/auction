7222a87d432268e9c30680cd0a7162e7d70feb64
Sindyukov Alexei <rodjer200586@gmail.com>
2018-03-18 00:05:16 +0400

    Update lots list

--- auction/urls.py
+++ auction/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ListView.as_view(), name='lots'),
-    path('<int:pk>/', views.DetailView.as_view()),
+    path('<int:pk>/', views.DetailView.as_view(), name='lot_detail'),
 ]
